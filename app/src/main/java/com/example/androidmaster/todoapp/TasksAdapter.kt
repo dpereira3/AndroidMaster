4505ad96d7aaa4befac7283edba99c46c3266109
Pereira Damian <dpereira3@abc.gob.ar>
2024-08-09 11:15:08 -0300

    Todo app 2

--- app/src/main/java/com/example/androidmaster/todoapp/TasksAdapter.kt
+++ app/src/main/java/com/example/androidmaster/todoapp/TasksAdapter.kt
@@ -5,7 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidmaster.R
 
-class TasksAdapter (private val tasks:List<Task>) : RecyclerView.Adapter<TasksViewHolder>() {
+class TasksAdapter (var tasks:List<Task>, private val onTaskSelected: (Int) -> Unit) : RecyclerView.Adapter<TasksViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TasksViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_todo_task, parent, false)
@@ -16,5 +16,7 @@ class TasksAdapter (private val tasks:List<Task>) : RecyclerView.Adapter<TasksVi
 
     override fun onBindViewHolder(holder: TasksViewHolder, position: Int) {
         holder.render(tasks[position])
+        holder.itemView.setOnClickListener{ onTaskSelected(position) }
+
     }
 }
--- app/src/main/java/com/example/androidmaster/todoapp/TodoActivity.kt
+++ app/src/main/java/com/example/androidmaster/todoapp/TodoActivity.kt
@@ -1,6 +1,11 @@
 package com.example.androidmaster.todoapp
 
+import android.app.Dialog
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,6 +14,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidmaster.R
 import com.example.androidmaster.todoapp.TaskCategory.*
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class TodoActivity : AppCompatActivity() {
 
@@ -16,18 +22,21 @@ class TodoActivity : AppCompatActivity() {
         Business,
         Personal,
         Other
-    )
+    ) //categorias a mostrar y utilizar
 
     private val tasks = mutableListOf(
         Task("Prueba Business", Business),
         Task("Prueba Personal", Personal),
         Task("Prueba Otros", Other)
-    )
+    ) // tareas de prueba para que cargue en la app
 
     private lateinit var rvCategories: RecyclerView
     private lateinit var categoriesAdapter: CategoriesAdapter
+
     private lateinit var rvTasks: RecyclerView
     private lateinit var tasksAdapter: TasksAdapter
+
+    private lateinit var fabAddTask:FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,21 +49,72 @@ class TodoActivity : AppCompatActivity() {
         }
         initComponent()
         initUI()
+        initListeners()
+    }
+
+    private fun initListeners() {
+        fabAddTask.setOnClickListener { showDialog() }
+    }
+
+    private fun showDialog(){
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_task)
+
+        val btnAddTask: Button = dialog.findViewById(R.id.btnAddTask)
+        val etTask: EditText = dialog.findViewById(R.id.etTask)
+        val rgCategory: RadioGroup = dialog.findViewById(R.id.rgCategory)
+
+        btnAddTask.setOnClickListener{
+            val currentTask = etTask.text.toString()
+            if(currentTask.isNotEmpty()){
+                val selectedId = rgCategory.checkedRadioButtonId
+                val selectedRadioButton:RadioButton = rgCategory.findViewById(selectedId)
+                val currentCategory:TaskCategory = when(selectedRadioButton.text){
+                    getString(R.string.todo_dialog_category_business) -> Business
+                    getString(R.string.todo_dialog_category_personal) -> Personal
+                    else -> Other
+                }
+                tasks.add(Task(currentTask, currentCategory))
+                updateTask()
+                dialog.hide()
+            }
+        }
+
+        dialog.show()
+        // cargar el dialogo para crear nuevas tareas
     }
 
     private fun initComponent(){
         rvCategories = findViewById(R.id.rvCategories)
         rvTasks = findViewById(R.id.rvTasks)
+        fabAddTask = findViewById(R.id.fabTask)
     }
 
     private fun initUI(){
-        categoriesAdapter = CategoriesAdapter(categories)
+        categoriesAdapter = CategoriesAdapter(categories) { position -> updateCategories(position) }
         rvCategories.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         rvCategories.adapter = categoriesAdapter
 
-        tasksAdapter = TasksAdapter(tasks)
+        tasksAdapter = TasksAdapter(tasks) { position -> onItemSelected(position) }
         rvTasks.layoutManager = LinearLayoutManager(this)
         rvTasks.adapter = tasksAdapter
-    }
+    } // carga las categorias y tareas en la pantalla
 
-}
+    private fun onItemSelected(position:Int){
+        tasks[position].isSelected = !tasks[position].isSelected
+        updateTask()
+    } // selecciona la tarea para marcarla
+
+    private fun updateCategories(position: Int){
+        categories[position].isSelected = !categories[position].isSelected
+        categoriesAdapter.notifyItemChanged(position)
+        updateTask()
+    } // selecciona la categoria para filtrar las tareas
+
+    private fun updateTask(){
+        val selectedCategories: List<TaskCategory> = categories.filter { it.isSelected }
+        val newTasks = tasks.filter { selectedCategories.contains(it.category) }
+        tasksAdapter.tasks = newTasks
+        tasksAdapter.notifyDataSetChanged()
+    }
+} // al crear nuevas tareas se agregan a la lista, y actualiza la lista de tareas filtradas.
